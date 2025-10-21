ef8e645a35dfe7b7c267ca8ad78bfd51fbe9c433
Allen <allengeorge2400@gmail.com>
2025-10-21 23:15:23 +0000

    Added hello_solana project

---
+++ hello_solana/programs/hello_solana/src/lib.rs
@@ -0,0 +1,50 @@
+use anchor_lang::prelude::*;
+
+declare_id!("3L2UUNCduXAZsk8qtKtaEewnQPww8NMVrsowXa5nnUwP");
+
+#[program]
+pub mod hello_solana {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>,name: String) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        let greeting_account = &mut ctx.accounts.greeting_account;
+        greeting_account.greeting = format!("Hello, {}!", name);
+        greeting_account.name = name;
+        Ok(())
+    }
+
+    pub fn greet(ctx: Context<UpdateGreeting>, new_name: String) -> Result<()> {
+        let greeting_account = &mut ctx.accounts.greeting_account;
+        greeting_account.greeting = format!("Hello, {}!", new_name);
+        greeting_account.name = new_name;
+        msg!("Greeting Updated: {}", greeting_account.greeting);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 32 + 200 ,
+    )]
+    pub greeting_account: Account<'info, GreetingAccount>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateGreeting<'info> {
+    #[account(mut)]
+    pub greeting_account: Account<'info, GreetingAccount>,
+}
+
+#[account]
+pub struct GreetingAccount {
+    pub name: String,
+    pub greeting: String,
+}
